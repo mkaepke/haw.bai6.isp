91aed89ef66597f20da86f6657d6df7cf4dbf040
Marc Kaepke <marc.kaepke@gmail.com>
2016-10-03 18:33:37 +0200

    Abfragen zur Übung auf Seite 209 von "Logik"

---
+++ VorlesungsUebungen/unifikation.pl
@@ -0,0 +1,18 @@
+% Anfragen
+/*
+hallo = 'hallo'. 
+hallo = "hallo".
+hallo == 'hallo'. 
+5 = '5'.
+Hallo = 'hallo'.
+'Hallo' = 'hallo'
+X = 'hallo', X= 'Hallo'.
+X == Y.
+X = Y.
+foo(X,f(g)) = foo(f(g),Y).
+foo(X,f(g)) = foo(f(f(g)),f(Y)). 
+foo(X,f(g)) = foo(f(f(g)),f(X)).
+foo(X,f(g)) = foo(f(f(g)),f(_X)).
+foo(f(f(g)),X) = foo(f(X),f(g)). 
+foo(f(f(X)),_X) = foo(f(X),f(g)).
+*/
